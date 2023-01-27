4e609045627d48b155b24e3d6f27a79eeda839fe
basemelazab <basem.elazab2014@gmail.com>
2023-01-27 07:42:16 +0200

    Add project files.

---
+++ SampleAPI/Controllers/ContactController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SampleAPI.Models;
+
+namespace SampleAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ContactController : Controller
+    {
+        private readonly SampleDbContext sampleDbContext;
+        public ContactController(SampleDbContext sampleDbContext)
+        {
+            this.sampleDbContext = sampleDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllContacts()
+        {
+            var contacts = await sampleDbContext.Contacts.ToListAsync();
+            return Ok(contacts);
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        [ActionName("GetContact")]
+        public async Task<IActionResult> GetContact([FromRoute] int id)
+        {
+            var contact = await sampleDbContext.Contacts.FirstOrDefaultAsync(x => x.ContactId == id);
+            if (contact != null)
+            {
+                return Ok(contact);
+            }
+            return NotFound("Contact is not found");
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddContact([FromBody] Contact contact)
+        {
+            await sampleDbContext.Contacts.AddAsync(contact);
+            await sampleDbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetContact), new { id = contact.ContactId }, contact);
+        }
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IActionResult> UpdateContact([FromRoute] int id, [FromBody] Contact contact)
+        {
+            var exsistingContact = await sampleDbContext.Contacts.FirstOrDefaultAsync(x => x.ContactId == id);
+            if (exsistingContact != null)
+            {
+                exsistingContact.FirstName = contact.FirstName;
+                exsistingContact.LastName = contact.LastName;
+                exsistingContact.EmailAddress = contact.EmailAddress;
+                exsistingContact.Phone = contact.Phone;
+
+
+                await sampleDbContext.SaveChangesAsync();
+                return Ok(exsistingContact);
+            }
+            return NotFound("Contact is not found");
+        }
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IActionResult> DeleteContact([FromRoute] int id)
+        {
+            var exsistingContact = await sampleDbContext.Contacts.FirstOrDefaultAsync(x => x.ContactId == id);
+            if (exsistingContact != null)
+            {
+                sampleDbContext.Contacts.Remove(exsistingContact);
+                await sampleDbContext.SaveChangesAsync();
+                return Ok(exsistingContact);
+            }
+            return NotFound("Contact is  not found");
+        }
+    }
+}
+
---
+++ SampleAPI/Models/SampleDbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleAPI.Models;
+
+public partial class SampleDbContext : DbContext
+{
+    public SampleDbContext()
+    {
+    }
+
+    public SampleDbContext(DbContextOptions<SampleDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=SampleDB;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contact>(entity =>
+        {
+            entity.HasKey(e => e.ContactId).HasName("PK__Contact__5C66259B850077DD");
+
+            entity.ToTable("Contact");
+
+            entity.Property(e => e.EmailAddress)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Phone)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
